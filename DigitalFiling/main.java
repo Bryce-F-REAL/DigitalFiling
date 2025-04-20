5e80c6f8a263c0c2af2974819b3581d244269311
Bryce-F-REAL <bryce.feibus@gmail.com>
2025-04-20 15:05:53 -0400

    Moved to package (this is like learning bullshit!!!!)

---
+++ DigitalFiling/main.java
@@ -0,0 +1,16 @@
+package DigitalFiling;
+
+/**
+ *
+ * @author Bryce Feibus
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
