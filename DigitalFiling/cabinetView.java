73975a11b38d8ebde21c6bf46f06881237abae14
Bryce-F-REAL <bryce.feibus@gmail.com>
2025-04-27 16:29:42 -0400

    Update cabinetView.java

--- DigitalFiling/cabinetView.java
+++ DigitalFiling/cabinetView.java
@@ -1,5 +1,12 @@
 package DigitalFiling;
 
 public class cabinetView {
-    
+     Cabinet[] cabList = new Cabinet[64];
+
+    public void newCabinet(String name) { 
+        Cabinet newC = new Cabinet();
+        newC.setName(name);
+    }
+
+
 }
