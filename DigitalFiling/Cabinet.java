b249a31533419d12bbe812f3060c37ffc47b9b01
Bryce-F-REAL <bryce.feibus@gmail.com>
2025-04-27 15:52:35 -0400

    Update Cabinet.java

--- DigitalFiling/Cabinet.java
+++ DigitalFiling/Cabinet.java
@@ -1,6 +1,7 @@
 package DigitalFiling;
 
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Cabinet {
     private int ID;
@@ -21,6 +22,19 @@ public class Cabinet {
     private void setName(String na) {
         //method to set the name property
         this.Name = na;
+    }
+
+
+    private boolean unlock( String dataPass) { //method to check a password against the stored password for the purpose of unlocking
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter Password");
+    String pass = sc.nextLine();
+    if(dataPass.equals(pass)){ 
+        return true;
+    } else {
+        return false;
+    }
+
     }
 
 }
