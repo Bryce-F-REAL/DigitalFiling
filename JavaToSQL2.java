0c673dfef7b78751ced107d94d6928b9a857c8aa
Scorch-the-dragon <henrydragon129@gmail.com>
2025-04-28 14:54:15 -0400

    Add files via upload
    Version 2 with the getting info so far. My mind is too stressed to clearly figure it out the problems right now.  also the connecter file is included

---
+++ JavaToSQL2.java
@@ -0,0 +1,203 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javatosql2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+/**
+ *
+ * @author Henry Mutschler
+ */
+public class JavaToSQL2 {
+
+    /**
+     * @param args the command line arguments
+     */
+ public static void main(String[] args) {
+        // TODO code application logic here
+        String jdbcUrl = "jdbc:mysql://localhost:3306/cabinetdb";
+        String username = "root";
+        String password = "B00t!";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            // Now you can use 'connection' to execute SQL queries.
+            // Don't forget to close the connection when you're done.
+          //  String updateQuery = "INSERT INTO cabinet (CabinetID, CabinetName, DateOfCreation, CabinetLocked, Password, Owner, Filepath) VALUES(12, 'My dudes', '2025-12-21', 1, 'myNUTT!', 'Bosh', '/cabinets/Homework');";
+
+            String createTable = "CREATE TABLE cabinet ("
+                    + "CabinetID int (100) Primary key, "
+                    + "CabinetName VARCHAR(20), "
+                    + "DateOfCreation date, "
+                    + "CabinetLocked boolean, "
+                    + "Password Varchar(20), "
+                    + "Owner Varchar(20), "
+                    + "FilePath LONGTEXT)"; //untested
+            String createSchema = "CREATE SCHEMA myNewSchema"; //untested
+            String createDataBase = "CREATE DATABASE myDatabase"; //untested
+           
+          //  PreparedStatement preparedStatement2 = connection.prepareStatement(updateQuery);
+          //  preparedStatement2.executeUpdate();
+            //update to change, query to look
+            // executeUpdate and executeQuery
+            // ResultSet makes it so we can place the data elsewhere like a string
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+       public static void showAllCabinetNames(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("CabinetName");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+        public static void showAllCabinetIDs(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("CabinetID");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+           public static void showAllCabinetDate(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("DateOfCreation");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }     
+    
+       public static void showAllCabinetLockStat(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("CabinetLocked");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } 
+    
+         public static void showAllCabinetPassword(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("Password");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+  
+             public static void showAllCabinetOwner(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("Owner");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } 
+   
+                 public static void showAllCabinetFilePath(String jdbcUrl, String username, String password) {
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            String allQuery = "SELECT * FROM cabinet;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(allQuery);
+            ResultSet executeQuery = preparedStatement1.executeQuery();
+
+            while (executeQuery.next()) //How we get the results
+            {
+                String CabinetName = executeQuery.getString("FilePath");
+                // print the results
+                System.out.format("%s, \n", CabinetName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }           
+        
+ 
+}
