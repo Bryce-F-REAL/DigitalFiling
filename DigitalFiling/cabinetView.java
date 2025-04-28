c5c82b5de745bb2b393c78f594ce47f3008a9f6c
Bryce-F-REAL <bryce.feibus@gmail.com>
2025-04-28 14:17:48 -0400

    work
    umm moving methods to where they need to be and not dying in the process????

--- DigitalFiling/cabinetView.java
+++ DigitalFiling/cabinetView.java
@@ -6,13 +6,9 @@ import java.util.ArrayList;
 public class cabinetView {
      ArrayList<Cabinet> cabList = new ArrayList<Cabinet>();
      ArrayList<File> cabFiles = new ArrayList<File>();
-     Filepath location;
+     File location;
 
-    public void newCabinet(String name) { 
-        Cabinet newC = new Cabinet();
-        newC.setName(name);
-        cabList.add(newC);
-    }
+    
 
     public void initializeCabinet() {
         //essentially this method collects the files to display for this method
--- DigitalFiling/mainView.java
+++ DigitalFiling/mainView.java
@@ -1,6 +1,31 @@
 package DigitalFiling;
 
+import java.util.ArrayList;
 
 public class mainView {
+
+    ArrayList<Cabinet> cabList = new ArrayList<Cabinet>();
     
+
+
+    private void fillList() {
+        
+    }
+
+
+
+
+
+
+
+
+
+    private void newCabinet(String name, String owner) { 
+        Cabinet newC = new Cabinet();
+        newC.setName(name);
+        newC.setOwner(owner);
+        cabList.add(newC);
+    }
+
+
 }
